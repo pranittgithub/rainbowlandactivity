07b6fcd85b391a7947eae7a274275d7d3657372e
Aashu020 <aashunadaf070@gmail.com>
2025-04-14 17:14:12 +0530

    program pages done

--- assets/js/main.js
+++ assets/js/main.js
@@ -495,6 +495,27 @@ CSS TABLE OF CONTENTS
     }
 
     loader();
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const counters = document.querySelectorAll(".counter");
+    
+        counters.forEach(counter => {
+          counter.innerText = "0";
+          const updateCount = () => {
+            const target = +counter.getAttribute("data-target");
+            const count = +counter.innerText;
+            const increment = target / 100;
+    
+            if (count < target) {
+              counter.innerText = Math.ceil(count + increment);
+              setTimeout(updateCount, 20);
+            } else {
+              counter.innerText = target;
+            }
+          };
+          updateCount();
+        });
+      });
    
 
 })(jQuery); // End jQuery
